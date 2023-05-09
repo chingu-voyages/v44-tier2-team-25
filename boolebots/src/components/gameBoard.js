f68269c82c6436dd1ee1d78ad402e72bb8ddeb8b
“Matt <matt.s.demartino@gmail.com>
2023-05-09 11:38:53 -0400

    Add tentative styling

--- boolebots/src/components/gameBoard.js
+++ boolebots/src/components/gameBoard.js
@@ -2,13 +2,19 @@ import React from "react";
 
 const GameBoard = () => {
   //State for board game size
-  const [boardSize, changeBoardSize] = React.useState(4);
+  const [boardSize, changeBoardSize] = React.useState(8);
 
   //Function to create game Board
   const createBoard = () => {
     const board = [];
     for (let i = 0; i < boardSize * boardSize; i++) {
-      board.push(<div key={i}></div>);
+      board.push(
+        <div
+          key={i}
+          id={i}
+          style={{ border: "1px solid black", padding: "30px" }}
+        ></div>
+      );
     }
     return board;
   };
@@ -16,8 +22,9 @@ const GameBoard = () => {
   return (
     <div
       style={{
-        display: "grid",
+        display: "inline-grid",
         gridTemplateColumns: `repeat(${boardSize}, 1fr)`,
+        gap: "1px",
       }}
     >
       {createBoard()}
