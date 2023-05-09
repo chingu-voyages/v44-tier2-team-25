21a04c3bc8049a6eb6eb9b082b2f84c43decf68b
“Matt <matt.s.demartino@gmail.com>
2023-05-09 11:49:21 -0400

    Created more efficient createBoard function

--- boolebots/src/components/gameBoard.js
+++ boolebots/src/components/gameBoard.js
@@ -1,26 +1,43 @@
 import React from "react";
 
 const GameBoard = () => {
-  //State for board game size
+  //State for board game size. Should probably be a prop that is passed to this component.
   const [boardSize, changeBoardSize] = React.useState(8);
 
-  //Function to create game Board
+  //Old function to create game Board. Not as useful as new function down below.
   const createBoard = () => {
+    // const board = [];
+    // for (let i = 0; i < boardSize * boardSize; i++) {
+    //   board.push(
+    //     <div
+    //       key={i}
+    //       id={i}
+    //       style={{ border: "1px solid black", padding: "30px" }}
+    //     ></div>
+    //   );
+    // }
+
+    //This version allows for each square to have an id with it's coordinates. This will help when triggering a bot battle.
     const board = [];
-    for (let i = 0; i < boardSize * boardSize; i++) {
-      board.push(
-        <div
-          key={i}
-          id={i}
-          style={{ border: "1px solid black", padding: "30px" }}
-        ></div>
-      );
+    for (let row = 0; row < 8; row++) {
+      board.push([]);
+      for (let col = 0; col < 8; col++) {
+        board[row].push(
+          <div
+            key={`${col}${row}`}
+            //Temporary styling
+            style={{ border: "1px solid black", padding: "30px" }}
+            id={`${col + 1},${row + 1}`}
+          ></div>
+        );
+      }
     }
     return board;
   };
 
   return (
     <div
+      //Temporary styling to visualize board 
       style={{
         display: "inline-grid",
         gridTemplateColumns: `repeat(${boardSize}, 1fr)`,
