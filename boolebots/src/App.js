86abcc619f619913f71fbd8f6e6340a407f8424e
“Matt <matt.s.demartino@gmail.com>
2023-05-11 13:57:13 -0400

    Create testBot state

--- boolebots/src/App.js
+++ boolebots/src/App.js
@@ -2,10 +2,15 @@ import React from "react";
 import GameBoard from "./components/gameBoard";
 function App() {
   const [boardSize, changeBoardSize] = React.useState(8);
-
+  const [bot1, setbot1] = React.useState({
+    name: "bot1",
+    operator: "AND",
+    x: 3,
+    y: 4,
+  });
   return (
     <div className="App">
-      <GameBoard boardSize={boardSize}/>
+      <GameBoard boardSize={boardSize} bot1={bot1} />
     </div>
   );
 }
