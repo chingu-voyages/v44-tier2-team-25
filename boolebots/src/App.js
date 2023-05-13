08fc78fc2db605c71ea4ab954d6fce29d017595f
Paul Pessoa <paulmspessoa@gmail.com>
2023-05-13 18:14:48 -0300

    importing leaderboard component

--- boolebots/src/App.js
+++ boolebots/src/App.js
@@ -1,5 +1,16 @@
+import Leaderboard from './components/Leaderboard'
+
+const battleResults = [
+  {id: 1, name: 'Argonauts', battles: {win: 1, loss: 1, tie: 1}},
+  {id: 2, name: 'Blackhawks', battles: {win: 1, loss: 0, tie: 1}},
+  {id: 3, name: 'Dynamo', battles: {win: 1, loss: 2, tie: 1}},
+  {id: 4, name: 'Globetrotters', battles: {win: 5, loss: 0, tie: 1}}
+];
+
 function App() {
-  return <div className="App"></div>;
+  return <div className="App">
+    <Leaderboard battleResults={battleResults}/>
+  </div>;
 }
 
 export default App;
