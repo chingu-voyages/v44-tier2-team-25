12e42e08fb56e461855d41389af83ebe9e3f1c0d
Paul Pessoa <paulmspessoa@gmail.com>
2023-05-13 22:04:21 -0300

    install and config SASS

--- boolebots/src/App.js
+++ boolebots/src/App.js
@@ -1,9 +1,11 @@
-import Leaderboard from './components/Leaderboard'
+import Leaderboard from './components/LeaderBoard/Leaderboard'
+import './styles/global.scss';
 
+// mock results
 const battleResults = [
   {id: 1, name: 'Argonauts', battles: {win: 1, loss: 1, tie: 1}},
   {id: 2, name: 'Blackhawks', battles: {win: 1, loss: 0, tie: 1}},
-  {id: 3, name: 'Dynamo', battles: {win: 1, loss: 2, tie: 1}},
+  {id: 3, name: 'Dynamo', battles: {win: 0, loss: 2, tie: 0}},
   {id: 4, name: 'Globetrotters', battles: {win: 5, loss: 0, tie: 1}}
 ];
 
