507b0ad109a7062cd8970632d586e30a083f8960
“Matt <matt.s.demartino@gmail.com>
2023-05-15 11:00:08 -0400

    Remove old code

--- boolebots/src/components/gameBoard.js
+++ boolebots/src/components/gameBoard.js
@@ -1,21 +1,6 @@
 import React from "react";
 
 const GameBoard = ({ boardSize, bot1, bot2 }) => {
-  //State for board game size. Should probably be a prop that is passed to this component.
-
-  //Old function to create game Board. Not as useful as new function down below.
-  // const createBoard = () => {
-  // const board = [];
-  // for (let i = 0; i < boardSize * boardSize; i++) {
-  //   board.push(
-  //     <div
-  //       key={i}
-  //       id={i}
-  //       style={{ border: "1px solid black", padding: "30px" }}
-  //     ></div>
-  //   );
-  // }
-
   //This version allows for each square to have an id with it's coordinates. This will help when triggering a bot battle.
   const board = [];
   for (let row = 0; row < boardSize; row++) {
