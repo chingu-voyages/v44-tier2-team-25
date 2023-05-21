a8ffd7454e36e1a63f0fa8abf5bf82f205a09689
Matt DeMartino <102242515+demartinom@users.noreply.github.com>
2023-05-21 13:53:48 -0400

    Convert logic to switch

--- src/components/GameBoard/gameBoard.js
+++ src/components/GameBoard/gameBoard.js
@@ -12,16 +12,23 @@ const GameBoard = (props) => {
 
   const gameBoard = board.map((tile) => (
     <div className="tile" id={tile} key={tile}>
-      {(((`${props.bot1Data.x.toString()},${props.bot1Data.y.toString()}` ===
-        tile && <img src={props.bot1Data.image} alt="bot icon" />) ||
-        `${props.bot2Data.x.toString()},${props.bot2Data.y.toString()}` ===
-          tile) && <img src={props.bot2Data.image} alt="bot icon" />) ||
-        (((`${props.bot3Data.x.toString()},${props.bot3Data.y.toString()}` ===
-          tile && <img src={props.bot3Data.image} alt="bot icon" />) ||
-          `${props.bot4Data.x.toString()},${props.bot4Data.y.toString()}` ===
-            tile) && <img src={props.bot4Data.image} alt="bot icon" />)}
+      {(() => {
+        switch (tile) {
+          case `${props.bot1Data.x.toString()},${props.bot1Data.y.toString()}`:
+            return <img src={props.bot1Data.image} alt="bot 1" />;
+          case `${props.bot2Data.x.toString()},${props.bot2Data.y.toString()}`:
+            return <img src={props.bot2Data.image} alt="bot 2" />;
+          case `${props.bot3Data.x.toString()},${props.bot3Data.y.toString()}`:
+            return <img src={props.bot3Data.image} alt="bot 3" />;
+          case `${props.bot4Data.x.toString()},${props.bot4Data.y.toString()}`:
+            return <img src={props.bot4Data.image} alt="bot 4" />;
+          default:
+            return null;
+        }
+      })()}
     </div>
   ));
+
   return (
     <div className="board-container">
       <div
