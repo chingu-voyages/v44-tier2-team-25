083bde7885798958503b50e1e76cccefe32d4d88
Matt DeMartino <102242515+demartinom@users.noreply.github.com>
2023-05-21 13:34:02 -0400

    Create more unified prop names

--- src/components/GameBoard/gameBoard.js
+++ src/components/GameBoard/gameBoard.js
@@ -1,29 +1,32 @@
 import React from "react";
 import "./gameboard.scss";
 
-const GameBoard = ({ boardSize, bot1, bot2 }) => {
+const GameBoard = (props) => {
   //This version allows for each square to have an id with it's coordinates. This will help when triggering a bot battle.
   const board = [];
-  for (let row = 0; row < boardSize; row++) {
-    for (let col = 0; col < boardSize; col++) {
+  for (let row = 0; row < props.boardSize; row++) {
+    for (let col = 0; col < props.boardSize; col++) {
       board.push(`${col + 1},${row + 1}`);
     }
   }
+
   const gameBoard = board.map((tile) => (
     <div className="tile" id={tile} key={tile}>
-      {/* {((`${bot1.x.toString()},${bot1.y.toString()}` === tile && (
-        <img src={bot1.image} alt="bot icon" />
-      )) ||
-        `${bot2.x.toString()},${bot2.y.toString()}` === tile) && (
-        <img src={bot2.image} alt="bot icon" />
-      )} */}
+      {(((`${props.bot1Data.x.toString()},${props.bot1Data.y.toString()}` ===
+        tile && <img src={props.bot1Data.image} alt="bot icon" />) ||
+        `${props.bot2Data.x.toString()},${props.bot2Data.y.toString()}` ===
+          tile) && <img src={props.bot2Data.image} alt="bot icon" />) ||
+        (((`${props.bot3Data.x.toString()},${props.bot3Data.y.toString()}` ===
+          tile && <img src={props.bot3Data.image} alt="bot icon" />) ||
+          `${props.bot4Data.x.toString()},${props.bot4Data.y.toString()}` ===
+            tile) && <img src={props.bot4Data.image} alt="bot icon" />)}
     </div>
   ));
   return (
     <div className="board-container">
       <div
         className="game-board"
-        style={{ gridTemplateColumns: `repeat(${boardSize}, 1fr)` }}
+        style={{ gridTemplateColumns: `repeat(${props.boardSize}, 1fr)` }}
       >
         {gameBoard}
       </div>
