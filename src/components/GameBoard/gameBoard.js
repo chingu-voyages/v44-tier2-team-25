5609ae63687cbe477b824eaff4a3248694fbfb84
Matt DeMartino <102242515+demartinom@users.noreply.github.com>
2023-05-25 14:26:46 -0400

    Conditional to tell the bots to move or not

--- src/components/GameBoard/gameBoard.js
+++ src/components/GameBoard/gameBoard.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./gameboard.scss";
 import useInterval from "../../useInterval";
 
@@ -32,40 +32,42 @@ const GameBoard = (props) => {
     </div>
   ));
 
-  //handles random bot movement
+  // handles random bot movement
   useInterval(() => {
-    props.setBot1Data(moveBots(props.bot1Data));
-    props.setBot2Data(moveBots(props.bot2Data));
-    props.setBot3Data(moveBots(props.bot3Data));
-    props.setBot4Data(moveBots(props.bot4Data));
+    if (gameStatus) {
+      props.setBot1Data(moveBots(props.bot1Data));
+      props.setBot2Data(moveBots(props.bot2Data));
+      props.setBot3Data(moveBots(props.bot3Data));
+      props.setBot4Data(moveBots(props.bot4Data));
 
-    function moveBots(botData) {
-      const directions = ["up", "down", "left", "right"];
-      const randomDirection =
-        directions[Math.floor(Math.random() * directions.length)];
+      function moveBots(botData) {
+        const directions = ["up", "down", "left", "right"];
+        const randomDirection =
+          directions[Math.floor(Math.random() * directions.length)];
 
-      // this makes sure  the new coordinates are based on the random direction
-      let newX = botData.x;
-      let newY = botData.y;
+        // this makes sure  the new coordinates are based on the random direction
+        let newX = botData.x;
+        let newY = botData.y;
 
-      switch (randomDirection) {
-        case "up":
-          newY = Math.max(botData.y - 1, 1); // Ensure the new coordinate is within the board boundaries
-          break;
-        case "down":
-          newY = Math.min(botData.y + 1, props.boardSize); // Ensure the new coordinate is within the board boundaries
-          break;
-        case "left":
-          newX = Math.max(botData.x - 1, 1); // Ensure the new coordinate is within the board boundaries
-          break;
-        case "right":
-          newX = Math.min(botData.x + 1, props.boardSize); // Ensure the new coordinate is within the board boundaries
-          break;
-        default:
-          break;
+        switch (randomDirection) {
+          case "up":
+            newY = Math.max(botData.y - 1, 1); // Ensure the new coordinate is within the board boundaries
+            break;
+          case "down":
+            newY = Math.min(botData.y + 1, props.boardSize); // Ensure the new coordinate is within the board boundaries
+            break;
+          case "left":
+            newX = Math.max(botData.x - 1, 1); // Ensure the new coordinate is within the board boundaries
+            break;
+          case "right":
+            newX = Math.min(botData.x + 1, props.boardSize); // Ensure the new coordinate is within the board boundaries
+            break;
+          default:
+            break;
+        }
+
+        return { ...botData, x: newX, y: newY };
       }
-
-      return { ...botData, x: newX, y: newY };
     }
   }, 1000);
 
