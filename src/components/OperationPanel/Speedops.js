ef95366f02dee39c766662b6e445ad687c4fb4e0
heet-git <58705043+heet-git@users.noreply.github.com>
2023-05-28 15:09:12 -0400

    Update the dropdown component
    Update dropdown component and SCSS.

--- src/components/OperationPanel/Speedops.js
+++ src/components/OperationPanel/Speedops.js
@@ -1,7 +1,12 @@
 import React, {useState, useEffect} from "react";
+import { Box, Text, Slider, SliderTrack, SliderFilledTrack, SliderThumb, Select } from "@chakra-ui/react";
 import "./speedops.scss"
 
+
 function Speedops(){
+
+/* speed */
+
     const [speed, setSpeed] = useState(1)
 
 useEffect(() => {
@@ -12,33 +17,41 @@ useEffect(() => {
     return () => clearInterval(interval)
 } ,[speed])
 
+/* Opeation */
+
+    const [operation, setOperation] = useState("AND")
+
+const changeOps = (e) => {
+    setOperation(e.target.value)
+}
 
     return(
-        <div className="speedops">
-        <div>
-        <p className="input--title">Speed</p>
+        <Box className="speedops">
+        <Box>
+        <Text className="input--title">Speed</Text>
         <input type="range" 
         min="1" 
         max="3" 
         value= {speed}
         className="slider" 
-        onChange={(e) => setSpeed(parseInt(e.target.value))}
+        onChange={(e) => setSpeed(parseInt(e.target.value))} 
         />
-        <p className="speed--info">{speed === 1 ? "1x" : speed === 2 ? "2x" : "3x"}</p>
         
-        </div>
+        <Text className="speed--info">{speed === 1 ? "1x" : speed === 2 ? "2x" : "3x"}</Text>
+        
+        </Box>
 
-        <div>
-        <p className="input--title">Operation</p>
-        <select className="operation--drop">
+        <Box>
+        <Text className="input--title">Operation</Text>
+        <Select onChange={changeOps} size='sm' variant='filled'>
         <option value="AND">AND</option>
         <option value="OR">OR</option>
         <option value="XOR">XOR</option>
         <option value="NOT">NOT</option>
-        </select>
-        </div>
+        </Select>
+        </Box>
 
-        </div>
+        </Box>
     )
 }
 
