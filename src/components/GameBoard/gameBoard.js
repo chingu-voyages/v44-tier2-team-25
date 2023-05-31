d098beb90df9077275feabc03659ad73950b8a56
Matt DeMartino <102242515+demartinom@users.noreply.github.com>
2023-05-31 13:42:15 -0400

    Remove unneeded hook

--- src/components/GameBoard/gameBoard.js
+++ src/components/GameBoard/gameBoard.js
@@ -1,4 +1,4 @@
-import React, { useState, useContext, useEffect } from "react";
+import React, { useState, useContext } from "react";
 import { BotDataContext } from "../../App.js";
 import "./gameboard.scss";
 
