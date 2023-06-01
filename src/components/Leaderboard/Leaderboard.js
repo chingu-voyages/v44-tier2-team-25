fe0641eba50cf2ae1a8425293e204dd7b96045d8
Matt DeMartino <102242515+demartinom@users.noreply.github.com>
2023-06-01 13:02:16 -0400

    Leadearboard displays bot wins in order

--- src/components/Leaderboard/Leaderboard.js
+++ src/components/Leaderboard/Leaderboard.js
@@ -1,51 +1,40 @@
-import { useState, useEffect } from "react";
+import { useContext } from "react";
 import "./leaderboard.scss";
-import { Tag } from '@chakra-ui/react'
+import { Tag } from "@chakra-ui/react";
+import { BotDataContext } from "../../App";
 
 function Leaderboard({ battleResults }) {
-  const [results, setResults] = useState([]);
+  const { bot1Data, bot2Data, bot3Data, bot4Data, wins } =
+    useContext(BotDataContext);
 
-  useEffect(() => {
-    setResults(battleResults);
-  }, [battleResults]);
-
-  const calculateScore = (battles) => {
-    let score = 0;
-    for (let key in battles) {
-      score += battles[key];
-    }
-    return score;
+  //converts wins array into wins by bot
+  const addWins = (botName) => {
+    return wins.filter((result) => result === `winner: ${botName.name}`).length;
   };
 
-  const sortedResults = [...results].sort(
-    (a, b) => calculateScore(b.battles) - calculateScore(a.battles)
-  );
+  const results = [
+    { id: bot1Data.id, name: bot1Data.name, wins: addWins(bot1Data) },
+    { id: bot2Data.id, name: bot2Data.name, wins: addWins(bot2Data) },
+    { id: bot3Data.id, name: bot3Data.name, wins: addWins(bot3Data) },
+    { id: bot4Data.id, name: bot4Data.name, wins: addWins(bot4Data) },
+  ];
+
+  //Sorts results array by number of wins
+  const sortedResults = results.sort((a, b) => b.wins - a.wins);
 
   return (
     <div className="leaderboard">
       <div className="header">
-        <div className="col">Ranking</div>
-        <div className="col">Battles</div>
-        <div className="col"></div>
+        <div className="col">Name</div>
+        <div className="col">Wins</div>
       </div>
-      {sortedResults.map((result, index) => {
+      {sortedResults.map((gameBot, index) => {
         return (
-          <div key={result.id} className="row">
-            <div className="col-ranking">{result.name}</div>
-            <div className="col-battles">
-              {result.battles.loss !== 0 && (
-                <Tag className="result loss">{result.battles.loss} - Loss</Tag>
-              )}
-              {result.battles.tie !== 0 && (
-                <Tag className="result tie">{result.battles.tie} - Tie</Tag>
-              )}
-              {result.battles.win !== 0 && (
-                <Tag className="result win">{result.battles.win} - Win</Tag>
-              )}
+          <div key={gameBot.id} className="row">
+            <div className="col-ranking">
+              {gameBot.name.length === 0 ? "Name Your Bot!" : gameBot.name}
             </div>
-            <div className="col-positions" style={{ color: index === 0 && 'orange' }}>
-              {index + 1}º
-            </div>
+            <div className="col-positions">{gameBot.wins}</div>
           </div>
         );
       })}
