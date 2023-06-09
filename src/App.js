b31243fc43724ee83f22be73dd8cc35737fb7c4b
heet-git <58705043+heet-git@users.noreply.github.com>
2023-06-09 13:25:41 -0400

    Change default view option of leaderboard and config panel
    Change default view option of leaderboard and config panel

--- src/App.js
+++ src/App.js
@@ -58,8 +58,8 @@ function App() {
     icon: "./bot4.gif",
   });
 
-  const [showLeaderboard, setShowLeaderboard] = useState(false);
-  const [showPanel, setShowPanel] = useState(false); 
+  const [showLeaderboard, setShowLeaderboard] = useState(true);
+  const [showPanel, setShowPanel] = useState(true); 
 
   return (
     <div className="App">
